c5604dcf1e4b5b1b4af01e5682159eead9e95ee5
Diego Calanzone <xdiego.wp@hotmail.com>
2023-07-01 15:54:18 +0200

    fix

--- full_run.sh
+++ full_run.sh
@@ -1,11 +1,11 @@
-python run_arena.py --config configs/normal/0a.json
-python run_arena.py --config configs/normal/0b.json
-python run_arena.py --config configs/normal/0c.json
-python run_arena.py --config configs/normal/1c.json
-python run_arena.py --config configs/normal/2c.json
+python run_arena.py --config configs/normal/0a.json > norm_0a.json
+python run_arena.py --config configs/normal/0b.json > norm_0b.json
+python run_arena.py --config configs/normal/0c.json > norm_0c.json
+python run_arena.py --config configs/normal/1c.json > norm_1c.json
+python run_arena.py --config configs/normal/2c.json > norm_2c.json
 
-python run_arena.py --config configs/forced_coop/0a.json
-python run_arena.py --config configs/forced_coop/0b.json
-python run_arena.py --config configs/forced_coop/0c.json
-python run_arena.py --config configs/forced_coop/1c.json
-python run_arena.py --config configs/forced_coop/2c.json
+python run_arena.py --config configs/forced_coop/0a.json > coop_0a.json
+python run_arena.py --config configs/forced_coop/0b.json > coop_0b.json
+python run_arena.py --config configs/forced_coop/0c.json > coop_0c.json
+python run_arena.py --config configs/forced_coop/1c.json > coop_1c.json
+python run_arena.py --config configs/forced_coop/2c.json > coop_2c.json
